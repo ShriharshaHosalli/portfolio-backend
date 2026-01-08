b07fa3215a8cc6be485e326c7c886f6bb137b5f7
sanjana01935 <sanjanahosalli0669@gmail.com>
2026-01-08 18:55:31 +0530

    Fix CORS and contact API

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-  origin: "http://127.0.0.1:5500",
+  origin: "*",
   methods: ["GET", "POST"],
   allowedHeaders: ["Content-Type"]
 }));
